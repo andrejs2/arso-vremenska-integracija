ffb7b32b3e66b3be3f5421983fac8dd691365f25
Andrej <44124120+andrejs2@users.noreply.github.com>
2024-09-23 13:13:46 +0000

    update

--- venv/lib/python3.12/site-packages/homeassistant/components/axis/hub/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Internal functionality not part of HA infrastructure."""
-
-from .api import get_axis_api  # noqa: F401
-from .hub import AxisHub  # noqa: F401
--- venv/lib/python3.12/site-packages/homeassistant/components/cover/const.py
+++
@@ -1,3 +0,0 @@
-"""Constants for cover entity platform."""
-
-DOMAIN = "cover"
